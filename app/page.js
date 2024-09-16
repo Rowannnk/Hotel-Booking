021a6b86df7f027f44dfd7e9e11fbdc01b25d61d
rowan <u6511498@au.edu>
2024-09-16 12:44:15 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,104 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import React, { useState } from "react";
+import { DatePicker } from "antd";
+import "antd/dist/reset.css"; // Import Ant Design styles
+
+const { RangePicker } = DatePicker;
+
+const Home = () => {
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="container mx-auto p-5">
+      <div
+        className="bg-cover bg-center h-1/2-screen relative"
+        style={{
+          backgroundImage: `url('https://cdn.prod.website-files.com/5c6d6c45eaa55f57c6367749/65046bf150d1abb7e5911702_x-65046bcfdc4f0.webp')`,
+          height: "50vh",
+        }}
+      >
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="relative z-10 text-white text-center py-20">
+          <h1 className="text-5xl font-bold mb-8">Find Your Perfect Room</h1>
+          <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-4">
+            <div className="w-full md:w-auto">
+              <RangePicker
+                className="w-full h-10"
+                format="DD-MM-YYYY"
+                onChange={(dates) => {
+                  if (dates && dates.length === 2) {
+                    setFromDate(dates[0].format("DD-MM-YYYY"));
+                    setToDate(dates[1].format("DD-MM-YYYY"));
+                  } else {
+                    setFromDate("");
+                    setToDate("");
+                  }
+                }}
+              />
+            </div>
+            <div className="w-full md:w-auto">
+              <input
+                type="text"
+                className="w-full h-10 px-4 rounded-lg text-gray-800"
+                placeholder="Search Rooms"
+              />
+            </div>
+            <div className="w-full md:w-auto">
+              <select className="w-full h-10 px-4 rounded-lg text-gray-800">
+                <option value="all">All</option>
+                <option value="delux">Delux</option>
+                <option value="suite">Suite</option>
+              </select>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+
+      <div className="mt-10">
+        {/* Replace with static data for rooms */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+          <div className="border p-4 rounded-lg">
+            <img
+              src="https://t3.ftcdn.net/jpg/02/71/08/28/360_F_271082810_CtbTjpnOU3vx43ngAKqpCPUBx25udBrg.jpg"
+              alt="Delux Room"
+              className="w-full h-40 object-cover rounded-lg mb-4"
+            />
+            <h2 className="text-2xl font-bold">Delux Room</h2>
+            <p className="text-gray-600">Delux</p>
+            <p className="text-lg text-gray-900 font-semibold">
+              2000 Baht/night
+            </p>
+          </div>
+          <div className="border p-4 rounded-lg">
+            <img
+              src="https://t3.ftcdn.net/jpg/02/71/08/28/360_F_271082810_CtbTjpnOU3vx43ngAKqpCPUBx25udBrg.jpg"
+              alt="Delux Room"
+              className="w-full h-40 object-cover rounded-lg mb-4"
+            />
+            <h2 className="text-2xl font-bold">Delux Room</h2>
+            <p className="text-gray-600">Delux</p>
+            <p className="text-lg text-gray-900 font-semibold">
+              2000 Baht/night
+            </p>
+          </div>
+          <div className="border p-4 rounded-lg">
+            <img
+              src="https://t3.ftcdn.net/jpg/02/71/08/28/360_F_271082810_CtbTjpnOU3vx43ngAKqpCPUBx25udBrg.jpg"
+              alt="Delux Room"
+              className="w-full h-40 object-cover rounded-lg mb-4"
+            />
+            <h2 className="text-2xl font-bold">Delux Room</h2>
+            <p className="text-gray-600">Delux</p>
+            <p className="text-lg text-gray-900 font-semibold">
+              2000 Baht/night
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
---
+++ components/Navbar.jsx
@@ -0,0 +1,119 @@
+"use client";
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div>
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <a className="text-xl font-semibold text-gray-800" href="/home">
+                Hotel Rooms
+              </a>
+            </div>
+            <div className="flex items-center">
+              <div className="hidden md:block">
+                <ul className="flex items-center space-x-4">
+                  <div className="relative">
+                    <button
+                      className="text-gray-700 font-medium hover:text-gray-900 flex items-center"
+                      id="dropdownMenuButton"
+                      onClick={toggleDropdown}
+                    >
+                      {/* {user.name} */}I am Nigga
+                      <svg
+                        className={`ml-2 h-4 w-4 transform transition-transform duration-200 ${
+                          isDropdownOpen ? "rotate-90" : "rotate-0"
+                        }`}
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M5 12h14M12 5l7 7-7 7"
+                        />
+                      </svg>
+                    </button>
+                    {isDropdownOpen && (
+                      <div
+                        className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-10"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <a
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                          href="/profile"
+                        >
+                          Profile
+                        </a>
+                        <a
+                          className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                          href="#"
+                        >
+                          Logout
+                        </a>
+                      </div>
+                    )}
+                  </div>
+
+                  <>
+                    <li className="nav-item">
+                      <a
+                        className="text-gray-700 font-medium hover:text-gray-900"
+                        href="/register"
+                      >
+                        Register
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="text-gray-700 font-medium hover:text-gray-900"
+                        href="/login"
+                      >
+                        Login
+                      </a>
+                    </li>
+                  </>
+                </ul>
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                <button
+                  className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  <svg
+                    className="h-6 w-6"
+                    stroke="currentColor"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16m-7 6h7"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
